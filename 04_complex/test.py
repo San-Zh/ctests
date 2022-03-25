3e58d26dd62cf951dc7fe7561e6929fa568ebe2a
kelong@apps <klzhang@cnic.cn>
2022-03-25 17:50:38 +0800

    len max 9e8

--- 04_complex/test.py
+++ 04_complex/test.py
@@ -13,18 +13,50 @@ system('g++ -O3 complex_vector.cpp -o complextest_O3.bin')
 
 
 
+########## Normal
+# Warm
+eN = 5  # MaxLen = 9e+eN
 Vlen = 100
-for Log in range(2, 7):
+for Log in range(2, eN):
     Vlen *= 10
     for n in range(1, 10):
         NVlen = Vlen * n
-        
+        RunCmdNormal = "./complextest_normal.bin {0}".format(NVlen, log_nrml)
+        system(RunCmdNormal)
+
+
+# Run
+eN = 8  # MaxLen = 9e+eN
+Vlen = 100
+for Log in range(2, eN):
+    Vlen *= 10
+    for n in range(1, 10):
+        NVlen = Vlen * n
         RunCmdNormal = "./complextest_normal.bin {0} >> {1}".format(NVlen, log_nrml)
         print(RunCmdNormal)
         system(RunCmdNormal)
 
+########## O3
+# Warm
+eN = 5  # MaxLen = 9e+eN
+Vlen = 100
+for Log in range(2, eN):
+    Vlen *= 10
+    for n in range(1, 10):
+        NVlen = Vlen * n
+        RunCmdO3 = "./complextest_O3.bin     {0}".format(NVlen, log_O3)
+        system(RunCmdO3)
+
+# Run
+eN = 8  # MaxLen = 9e+eN
+Vlen = 100
+for Log in range(2, eN):
+    Vlen *= 10
+    for n in range(1, 10):
+        NVlen = Vlen * n
         RunCmdO3 = "./complextest_O3.bin     {0} >> {1}".format(NVlen, log_O3)
         print(RunCmdO3)
         system(RunCmdO3)
+
 
         # RunCmd = f"./test {NVlen} 2>&1 | tee log"
