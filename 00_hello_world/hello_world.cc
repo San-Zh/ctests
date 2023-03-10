50ea97c80db55d363a55120ff06c0107bdad276d
kelong@cnic <klzhang@cnic.cn>
2023-03-10 09:23:52 +0800

    update

--- 00_hello_world/hello_world.cc
+++ 00_hello_world/hello_world.cc
@@ -1,14 +1,34 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 
 using namespace std;
 
-#define FOR12(e){e e e e e e e e e e e e}
+#define FOR12(e)                                                                                   \
+    {                                                                                              \
+        e e e e e e e e e e e e                                                                    \
+    }
 
-int main(int argc, char ** argv )
+template <int N>
+using ARRAY = int[N];
+
+template <int N>
+void array(ARRAY<N> &in)
 {
-    cout << "hello world!"<<endl;
-    cout << INT_MAX <<endl;
+    cout << "sizeof(in) = " << sizeof(in[N]) << endl;
+    for (size_t i = 0; i < sizeof(in) / sizeof(int); i++) {
+        cout << "a[" << i << "]" << in[i] << endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    cout << "hello world!" << endl;
+    cout << INT_MAX << endl;
     FOR12(cout << "12 hello" << endl;);
+
+    cout << "\narray[]\n" << endl;
+    ARRAY<4> a = {1, 2, 3, 4};
+    cout << "sizeof(a) = " << sizeof(a) << endl;
+    array(a);
     return 0;
 }
---
+++ 11_memory/memoryAccess.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <complex>
+#include <string.h>
+
+#define SIZE 1024 * 1024
+
+using DataTp = double;
+using namespace std;
+
+struct timeval start;
+struct timeval timer_end;
+
+#define timer(time_ms, code)                                                                       \
+    gettimeofday(&start, NULL);                                                                    \
+    code;                                                                                          \
+    gettimeofday(&timer_end, NULL);                                                                \
+    time_ms = (timer_end.tv_sec - start.tv_sec) * 1000000 + (timer_end.tv_usec - start.tv_usec);
+
+int main(int argc, char **argv)
+{
+    DataTp *A = (DataTp *) malloc(SIZE * sizeof(DataTp));
+    DataTp *B = (DataTp *) malloc(SIZE * sizeof(DataTp));
+    size_t *idx = (size_t *) malloc(SIZE * sizeof(size_t));
+
+    memset(A, 0, SIZE * sizeof(DataTp));
+    memset(B, 0, SIZE * sizeof(DataTp));
+
+    for (size_t i = 0; i < SIZE; i++) {
+        idx[i] = (i % 2) * SIZE / 2 + i / 2;
+    }
+
+    DataTp invRandMax = 1.0 / RAND_MAX;
+    for (size_t loop = 0; loop < 100; loop++) {
+        for (size_t i = 0; i < SIZE; i++) {
+            A[i] = invRandMax * (DataTp) rand();
+        }
+        for (size_t i = 0; i < SIZE; i++) {
+            B[i] = A[idx[i]];
+        }
+        for (size_t i = 0; i < SIZE; i++) {
+            A[i] = invRandMax * (DataTp) rand();
+        }
+        for (size_t i = 0; i < SIZE; i++) {
+            int eoidx = (i % 2) * SIZE / 2 + i / 2;
+            B[i] = A[eoidx];
+        }
+    }
+
+    double time0;
+    double time1;
+    {
+        for (size_t i = 0; i < SIZE; i++) {
+            A[i] = invRandMax * (DataTp) rand();
+        }
+        timer(time1, {
+            for (size_t i = 0; i < SIZE; i++) {
+                B[i] = A[idx[i]];
+            }
+        });
+    }
+    {
+        for (size_t i = 0; i < SIZE; i++) {
+            A[i] = invRandMax * (DataTp) rand();
+        }
+        timer(time0, {
+            for (size_t i = 0; i < SIZE; i++) {
+                int eoidx = (i % 2) * SIZE / 2 + i / 2;
+                B[i] = A[eoidx];
+            }
+        });
+    }
+
+    cout << "time0 = " << time0 << ",  time1 = " << time1 << endl;
+    free(A);
+    free(B);
+    free(idx);
+}
